6a6668da8531786a063dac00f800e0cee3a5f428
Faustino <faustino.garcia@agilethought.com>
2022-05-16 17:28:03 -0500

    Add listing and pagination

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -5,7 +5,7 @@ class ChefsController < ApplicationController
     end
 
     def index 
-        @chefs = Chef.all
+        @chefs = Chef.paginate(page: params[:page], per_page: 5)
     end
 
     def create
@@ -20,6 +20,9 @@ class ChefsController < ApplicationController
 
     def show
         @chef = Chef.find(params[:id])
+        @chef_recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
+        #A collection o 5 recipes per page, we are using this in order to show 3 recipes 
+        #per page on the chefs recipes show
     end
 
     def edit 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
     validates :name, presence: true
-    validates :description, presence: true, length: {minimum: 5, maximum: 500}
+    validates :description, presence: true, length: {minimum: 5, maximum: 2000}
     belongs_to :chef
     validates :chef_id, presence: true
+    default_scope -> { order(updated_at: :desc)}
 end
