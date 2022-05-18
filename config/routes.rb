9050d9c54488c3d027e21de10358325dba9e405b
Faustino <faustino.garcia@agilethought.com>
2022-05-18 01:16:41 -0500

    Complete comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
   # get '/recipes/:id', to: 'recipes#show', as: 'recipe'
   # post '/recipes', to: 'recipes#create'
-  resources :recipes
+  resources :recipes do
+    resource :comments, only: [:create]
+  end
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
   get '/login',to: 'sessions#new'
